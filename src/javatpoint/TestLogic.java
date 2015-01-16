f24c7dee9f5cad934803329ec3e8b00888813fac
unknown <ritus@ritus.cybage.com>
2015-01-16 13:03:36 +0530

    first commit

---
+++ src/javatpoint/TestLogic.java
@@ -0,0 +1,16 @@
+package javatpoint;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestLogic {
+	
+	
+	 @Test  
+	    public void testFindMax(){  
+	        assertEquals(4,Calculation.findMax(new int[]{1,3,4,2}));  
+	        assertEquals(-1,Calculation.findMax(new int[]{-12,-1,-3,-4,-2}));  
+	    }  
+
+}
